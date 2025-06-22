e52f5f6a85909cb4c86fdf263d8cfd41f2f03447
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2025-06-22 20:49:49 +0300

    chore: removed footer buttons

--- site/components/footer.tsx
+++ site/components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { CommandIcon, HeartIcon, TriangleIcon } from "lucide-react";
+import {HeartIcon, TriangleIcon } from "lucide-react";
 import { Dictionary } from "@/lib/dictionaries";
 
 export function Footer({ dict }: { dict: Dictionary }) {
@@ -8,29 +8,14 @@ export function Footer({ dict }: { dict: Dictionary }) {
     <footer className="border-t w-full h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
         <div className="flex items-center gap-3">
-          <CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
           <p className="text-center">
-            {dict.footer.built_by}{" "}
-            <Link
-              className="px-1 underline underline-offset-2"
-              href="https://github.com/nisabmohd"
-            >
-              nisabmohd
-            </Link>
-            . {dict.footer.source_code_available}{" "}
-            <Link
-              className="px-1 underline underline-offset-2"
-              href="https://github.com/nisabmohd/Aria-Docs"
-            >
-              GitHub
-            </Link>
-            .
+            All Copyrights &copy; {new Date().getFullYear()}{" "} are reserved by {" Refine AI"}
           </p>
         </div>
 
-        <div className="gap-4 items-center hidden md:flex">
+        {/* <div className="gap-4 items-center hidden md:flex">
           <FooterButtons dict={dict} />
-        </div>
+        </div> */}
       </div>
     </footer>
   );
