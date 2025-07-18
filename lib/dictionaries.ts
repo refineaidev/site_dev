2d4ad58a67a7c2fdf346a780fc1ebdb62af35b09
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2025-07-19 01:16:51 +0300

    added ar langauge

--- lib/dictionaries.ts
+++ lib/dictionaries.ts
@@ -5,8 +5,7 @@ export type LangProps = { params: Promise<{ lang: Locale }> };
 
 const dictionaries = {
   en: () => import("@/dictionaries/en.json").then((module) => module.default),
-  ja: () => import("@/dictionaries/ja.json").then((module) => module.default),
-  fr: () => import("@/dictionaries/fr.json").then((module) => module.default),
+  ar: () => import("@/dictionaries/ar.json").then((module) => module.default),
 };
 
 const getDictionaryUncached = async (locale: Locale) => dictionaries[locale]();
