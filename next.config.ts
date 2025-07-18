eceecbfaff881164f4f8fe9353986be6e3f8bf13
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2025-07-18 23:59:56 +0300

    build

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com/**",
+      },
+    ],
+  },
+  // if used turbopack
+  // transpilePackages: ["next-mdx-remote"],
+};
+
+export default nextConfig;
